78099363dd58f2edf8171e7eb010ddee4d371398
IreneCreadora <nargiza.dancer@gmail.com>
2022-10-14 17:48:12 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,45 +1,130 @@
 import Notiflix, { Notify } from 'notiflix';
-import { notifyOptions } from './notify-options';
-const axios = require('axios').default;
+import { notifyOptions } from './js/notify-options';
+import { createMarkup } from './js/createMarkup';
+import { PixabayAPI } from './js/pixabayAPI';
+import { refs } from './js/refs';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Make a request for a user with a given ID
-axios
-  .get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
+const pixabay = new PixabayAPI();
+
+const options = {
+  root: null,
+  rootMargin: '100px',
+  threshold: 1.0,
+};
+
+const callback = async function (entries, observer) {
+  entries.forEach(async entry => {
+    if (entry.isIntersecting && entry.intersectionRect.bottom > 550) {
+      // if (entry.isIntersecting) {
+      pixabay.incrementPage();
+      observer.unobserve(entry.target);
+
+      try {
+        const { hits } = await pixabay.getImages();
+        const markup = [...hits].map(createMarkup);
+        refs.gallery.insertAdjacentHTML('beforeend', markup.join(''));
+        createLightBox();
+        lightBox.refresh();
+
+        if (pixabay.isShowLoadMore) {
+          const target = document.querySelector('.gallery-card:last-child');
+          io.observe(target);
+        }
+      } catch (error) {
+        Notify.failure(`${error.message}`, notifyOptions);
+        clearPage();
+      }
+    }
   });
+};
+const io = new IntersectionObserver(callback, options);
 
-// Optionally the request above could also be done as
-axios
-  .get('/user', {
-    params: {
-      ID: 12345,
-    },
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+async function onFormSubmit(e) {
+  e.preventDefault();
 
-// Want to use async/await? Add the `async` keyword to your outer function/method.
-async function getUser() {
-  try {
-    const response = await axios.get('/user?ID=12345');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
+  const {
+    elements: { searchQuery },
+  } = e.currentTarget;
+  const userQuery = searchQuery.value.trim().toLowerCase().split(' ').join('+');
+  if (!userQuery) {
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`,
+      notifyOptions
+    );
+    return;
   }
+  pixabay.query = userQuery;
+
+  clearPage();
+
+  try {
+    const { hits, totalHits } = await pixabay.getImages();
+    if (hits.length === 0) {
+      Notify.info(
+        `Sorry, there are no images matching your search query "${userQuery}". Please try again.  `,
+        notifyOptions
+      );
+      return;
+    }
+    const markup = [...hits].map(createMarkup);
+    refs.gallery.insertAdjacentHTML('beforeend', markup.join(''));
+    createLightBox();
+
+    pixabay.calculateTotalPages(totalHits);
+
+    Notify.success(
+      `Hooray! We found ${totalHits} images by ${userQuery}`,
+      notifyOptions
+    );
+
+    if (pixabay.isShowLoadMore) {
+      const target = document.querySelector('.gallery-card:last-child');
+      console.log(target);
+
+      io.observe(target);
+    }
+  } catch (error) {
+    Notify.failure(`${error.message}`, notifyOptions);
+    clearPage();
+  }
+}
+
+function onLoadMore() {
+  pixabay.incrementPage();
+
+  if (!pixabay.isShowLoadMore) {
+    Notify.info(
+      `We're sorry, but you've reached the end of search results.`,
+      notifyOptions
+    );
+    refs.loadMoreBtn.classList.add('is-hidden');
+  }
+
+  pixabay
+    .getImages()
+    .then(({ hits }) => {
+      const markup = [...hits].map(createMarkup);
+      refs.gallery.insertAdjacentHTML('beforeend', markup.join(''));
+      lightBox.refresh();
+    })
+    .catch(error => {
+      Notify.failure(`${error.message}`, notifyOptions);
+      clearPage();
+    });
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function clearPage() {
+  pixabay.resetPage();
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+}
+
+function createLightBox() {
+  lightBox = new SimpleLightbox('.gallery a');
 }
