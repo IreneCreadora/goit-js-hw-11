b2fd95209f1ca62685b23dc332bad2c3076d9125
IreneCreadora <nargiza.dancer@gmail.com>
2022-10-14 17:47:32 +0300

    add js

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('[type="text"]'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
